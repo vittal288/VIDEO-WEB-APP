2660a8bba6b2a26a20a8dfbc8953d57ccb9a3acc
Vittal KAMKAR <vittal.kamkar@amadeus.com>
2020-08-05 21:00:12 +0530

    added the comments

--- public/script.js
+++ public/script.js
@@ -1,60 +1,77 @@
-const socket = io("/");
+// we need to pass the server path , i.e set up to / 
+// socket will connect to root path i.e /
+const socket = io('/');
 // store the joined users info
 const peers = {};
 //----------------------------------------------------------
 
 const videoGrid = document.getElementById("video-grid");
 
+// send our server information to the peerJS()
+// after doing this now, we have connections between the peerjs which is running on 3001 port 
 const myPeer = new Peer(undefined, {
   host: "/",
   port: "3001",
 });
 
 const myVideo = document.createElement("video");
+// mute ourselves 
 myVideo.muted = true;
+// trying to connect our video 
 navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true,
-  })
-  .then((stream) => {
+}).then((stream) => {
     addVideoStream(myVideo, stream);
-
+    // if someone tries to call 
     // someone tried to call the room 
     myPeer.on("call", call => {
       call.answer(stream);
       // new user get the current video streams
       const video = document.createElement('video');
       call.on('stream', userVideoStream => {
-        addVideoStream(video, userVideoStream)
+        addVideoStream(video, userVideoStream);
       })
     });
 
+    // if user is connecting to existing room and with on going stream 
     // user connected event 
     socket.on("user-connected", userId => {
       // if new user joined our room send the current stream to the new user 
       connectToNewUser(userId, stream);
     });
-  });
+});
 
-  // user leaves the room or disconnected
+// user leaves the room or disconnected
 socket.on("user-disconnected", (userId) => {
-  console.log(userId);
+  console.log(userId+' user is disconnected or left');
   if (peers[userId]) {
     peers[userId].close();
   }
 });
 
-myPeer.on("open", (id) => {
+
+
+// as soon we connect peer server and pass the id to server 
+myPeer.on("open", id => {
+  // it will emit an event to server 
+  // ROOM_ID is the id which was emitted from server and was captured in view room.ejs code 
   socket.emit("join-room", ROOM_ID, id);
 });
 
+
+// listen the event which is emitted from server 
 socket.on("user-connected", (userId) => {
   console.log("User connected", userId);
 });
+
+
 function addVideoStream(video, stream) {
+  // stream is playing video data 
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
+    // once video is loaded on the page , as soon we get the data and then play it
     video.play();
   });
 
@@ -62,11 +79,15 @@ function addVideoStream(video, stream) {
 }
 
 function connectToNewUser(userId, stream) {
+  // call method will call to the user, which is provided as userId
   const call = myPeer.call(userId, stream);
   const video = document.createElement("video");
+  // while calling , send the current vide stream 
+  // userVideoStream : this is the stream which other user is calling 
   call.on("stream", (userVideoStream) => {
     addVideoStream(video, userVideoStream);
   });
+  // if some leaves the call or disconnected the call , then remove that video from current room
   call.on("close", () => {
     video.remove();
   });
--- server.js
+++ server.js
@@ -1,29 +1,40 @@
 const express = require("express");
-const e = require("express");
 const app = express();
 const server = require("http").Server(app);
+// socket io will get to which server to communicate 
 const io = require("socket.io")(server);
+// generate dynamic room ids on press enter on the browser URL 
 const { v4: uuidv4 } = require("uuid");
-const { Socket } = require("net");
+
 //-------------------------------------------------------
 
+// for view engine, you can use it here any view engine 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+// set the home page if required, currently we dont have here 
 app.get("/", (req, res) => {
+  // generate dynamic room ids
   res.redirect(`/${uuidv4()}`);
 });
 
+// handle dynamic room id 
 app.get("/:room", (req, res) => {
+  // room is view name i.e (room.ejs)
   res.render("room", {
     roomId: req.params.room,
   });
 });
 
+
+// any time if someone connect to our web page 
+// through socket user connection happens 
 io.on('connection', socket=>{
+  // if some connects to our room
+  // join-room is event 
     socket.on('join-room', (roomId, userId)=>{
-        // console.log(roomId, userId);
         socket.join(roomId);
+        // send a message to the room (using broadcast, will send a message to everyone inside room)
         socket.to(roomId).broadcast.emit('user-connected', userId);
 
         // emit events once user disconnected 
@@ -32,5 +43,13 @@ io.on('connection', socket=>{
         });
     });
 });
+
+// start the server with port 3000
+// the video stream which just transfer to network data not with this server 
+// server is used just for ROOM 
 // UI port : http://localhost:3000
 server.listen(3000);
+
+
+
+// NOTE: PEERJS will helps us : they expose method to connections between the users using webRTC
